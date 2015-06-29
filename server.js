a161ac9b14e37b21d2de445ab61ebcbc33db83dc
Michael Tran <koramaiku@Michaels-MacBook-Pro-2.local>
2015-06-29 11:36:25 +1000

    Commpleted 3.2

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express'),
     api = require('./api'),
+    users = require('./accounts'),
     app = express();
 
 app
     .use(express.static('./public'))
+    .use(users)
     .use('/api', api)
     .get('*', function(req, res){
-       res.sendfile('public/main.html');
+        console.log(req.user);
+        if(!req.user){
+            res.redirect('/login')
+        } else {
+            res.sendfile('public/main.html');
+        }
     }).listen(5000);
